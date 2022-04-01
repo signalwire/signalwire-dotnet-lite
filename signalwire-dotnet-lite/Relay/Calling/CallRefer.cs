29ce4b26732b03b1161b24dceefd33310d12c53c
Kevin Garabedian <kevin.garabedian@signalwire.com>
2022-04-01 11:13:34 -0400

    adding relay sip refer support

---
+++ signalwire-dotnet-lite/Relay/Calling/CallRefer.cs
@@ -0,0 +1,17 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalWire.Relay.Calling
+{
+    public sealed class CallRefer
+    {
+        [JsonProperty("params", Required = Required.Always)]
+        public object Parameters { get; set; }
+
+        public T ParametersAs<T>() { return Parameters == null ? default(T) : ((JObject)Parameters).ToObject<T>(); }
+    }
+}
---
+++ signalwire-dotnet-lite/Relay/Calling/CallReferDevice.cs
@@ -0,0 +1,26 @@
+﻿using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
+using Newtonsoft.Json.Linq;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace SignalWire.Relay.Calling
+{
+    public sealed class CallReferDevice
+    {
+        [JsonProperty("type", Required = Required.Always)]
+        public string Type { get; set; } = "sip";
+
+        public sealed class Params
+        {
+            [JsonProperty("to", Required = Required.Always)]
+            public string To { get; set; }
+
+            [JsonProperty("headers", Required = Required.Always)]
+            public string Headers { get; set; }
+        }
+
+       // public T ParametersAs<T>() { return Parameters == null ? default(T) : (Parameters as JObject).ToObject<T>(); }
+    }
+}
