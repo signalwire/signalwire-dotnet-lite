e3335ac2224952ef60507e6957d7d9f69b7b79fe
Shane Bryldt <shane@signalwire.com>
2023-11-28 17:16:41 +0000

    Adjust packaging script to detect missing laml directory

--- signalwire-dotnet-lite/package.sh
+++ signalwire-dotnet-lite/package.sh
@@ -1,4 +1,9 @@
 export DOTNET_SYSTEM_NET_HTTP_USESOCKETSHTTPHANDLER=0
 rm -rf *.nupkg
+if [ ! -d "laml" ]; then
+    echo "Missing laml directory, run bootstrap.sh"
+    exit
+fi
+
 dotnet pack -c Release -o .
 dotnet nuget push `ls *.nupkg` -k $NUGET_APIKEY -s https://api.nuget.org/v3/index.json
